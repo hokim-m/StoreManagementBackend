835bf942da015cfd168e410cc6587d9ff0740008
Khokim Mamarasulov <alfacms.hm@gmail.com>
2018-08-06 17:18:54 +0500

    [StoreSchema]: Added parent property

--- model/schemas/store.js
+++ model/schemas/store.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const ObjectId = Schema.ObjectId;
 let StoreSchema = new Schema({
         title: String,
         group: String, //main, sub
         description: String,
+        parent: ObjectId,
         timestamp: Number
 }, {
         versionKey: false
