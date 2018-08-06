3bdaa94ab0edf01eba7f266304440cd3d33c5019
Khokim Mamarasulov <alfacms.hm@gmail.com>
2018-08-06 15:32:12 +0500

    [Application]: Added route to update given user data by its id

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-let express = require('express');
-let router  = express.Router();
+let express           = require('express');
+let router            = express.Router();
 const UsersCollection = require('../model/schemas/user');
-const Response = require('../views/response');
-const ObjectId = require('mongoose').Schema.Types.ObjectId;
+const Response        = require('../views/response');
+const ObjectId        = require('mongoose').Schema.Types.ObjectId;
 
 
 /* GET users listing. */
@@ -13,15 +13,39 @@ router.get('/', function (req, res, next) {
 router.post('/add', function (req, res) {
         let body = req.body;
         if (!body.hash) {
-                return Response.ErrorWithCodeAndMessage(res, -1, "HASH INPUT IS REQUIRED");
+                return Response.ErrorWithCodeAndMessage(res, -1, 'HASH INPUT IS REQUIRED');
         }
         if (!body.login) {
-                return Response.ErrorWithCodeAndMessage(res, -1, "LOGIN INPUT IS REQUIRED");
+                return Response.ErrorWithCodeAndMessage(res, -1, 'LOGIN INPUT IS REQUIRED');
         }
         let user = new UsersCollection(body);
-        user.save((err, onSave)=> {
+        user.save((err, onSave) => {
                 if (!err) {
                         Response.setData(res, onSave);
+                } else {
+                        Response.UNHANDLED_ERROR(res);
+                }
+        });
+
+});
+router.post('/update', function (req, res) {
+        const userId       = req.body.userId;
+        const user         = req.body.user;
+        let updatingParams = {$set: {}};
+        if (!user) {
+                return Response.ErrorWithCodeAndMessage(res, -1, 'Property `user` is required!');
+        }
+        if (!userId) {
+                return Response.ErrorWithCodeAndMessage(res, -1, 'Property `userId` is required!');
+        }
+
+        Object.keys(user).map(key => {
+                updatingParams['$set'][key] = user[key];
+        });
+
+        UsersCollection.update({_id: ObjectId}, updatingParams, function (err, result) {
+                if (!err) {
+                        Response.setData(res, result);
                 } else {
                         Response.UNHANDLED_ERROR(res);
                 }
@@ -33,7 +57,7 @@ router.get('/remove/:id', function (req, res) {
         try {
                 let obj = ObjectId(userId);
         } catch (e) {
-                Response.ErrorWithCodeAndMessage(res, -1, "Incorrect ObjectId passed as user id");
+                Response.ErrorWithCodeAndMessage(res, -1, 'Incorrect ObjectId passed as user id');
         }
         UsersCollection.remove({_id: ObjectId(userId)}, function (err, result) {
                 if (!err) {
@@ -41,7 +65,7 @@ router.get('/remove/:id', function (req, res) {
                 } else {
                         Response.UNHANDLED_ERROR(res);
                 }
-        })
+        });
 });
 router.get('/list', function (req, res) {
         UsersCollection.aggregate([
@@ -59,7 +83,7 @@ router.get('/list', function (req, res) {
                 } else {
                         Response.UNHANDLED_ERROR(res);
                 }
-        })
+        });
 });
 
 module.exports = router;
