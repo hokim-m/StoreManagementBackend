8d96cefa8659b9bee3b870b788f83e7c08791562
Khokim Mamarasulov <alfacms.hm@gmail.com>
2018-08-06 15:12:40 +0500

    [Application]: Created user schema

---
+++ model/schemas/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+        login: String,
+        name: String,
+        group: Number,
+        hash: String
+}, {
+        versionKey: false
+});
+
+const UserModel = mongoose.model('users', UserSchema);
+
+module.exports = UserModel;
