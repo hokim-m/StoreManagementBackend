7c0a865dae9b37eb20d4932fe325964b026d8e6a
Khokim Mamarasulov <alfacms.hm@gmail.com>
2018-08-06 15:24:49 +0500

    [Application]: Created comment response handler. [Users]: Added method to create new user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router  = express.Router();
+const UsersCollection = require('../model/schemas/user');
+const Response = require('../views/response');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+        res.send('respond with a resource');
+});
+
+router.post('/add', function (req, res) {
+        let body = req.body;
+        if (!body.hash) {
+                return Response.ErrorWithCodeAndMessage(res, -1, "HASH INPUT IS REQUIRED");
+        }
+        if (!body.login) {
+                return Response.ErrorWithCodeAndMessage(res, -1, "LOGIN INPUT IS REQUIRED");
+        }
+        let user = new UsersCollection(body);
+        user.save((err, onSave)=> {
+                if (!err) {
+                        Response.setData(res, onSave);
+                } else {
+                        Response.UNHANDLED_ERROR(res);
+                }
+        })
+
 });
 
 module.exports = router;
---
+++ views/response.js
@@ -0,0 +1,62 @@
+const CODES   = {
+        'OK'               : 0,
+        'UNHANDLED_ERROR'  : -1,
+        'TERMINATE_SESSION': -2
+};
+
+/**
+ * Response Model. Overwriting the data in to
+ * constructed way
+ * {
+ *      data: /some data, {Object}
+ *      meta: {
+ *              code: /code {Number}
+ *              message: /status {String}
+ *              }
+ *  }
+ * @constructor
+ */
+function Response () {
+        this.code = CODES;
+}
+/**
+ *
+ * @param res {Response} Express JS res method
+ * @param data {Object}
+ */
+Response.prototype.setData = function (res, data) {
+        this.pFillResponse(res, data, CODES.OK);
+};
+/**
+ * @requires res
+ * @param res
+ * @constructor
+ */
+Response.prototype.TERMINATE_SESSION = function (res) {
+        this.pFillResponse(res, null, CODES.TERMINATE_SESSION);
+};
+Response.prototype.UNHANDLED_ERROR   = function (res) {
+        this.pFillResponse(res, null, CODES.UNHANDLED_ERROR);
+};
+Response.prototype.pFillResponse     = function (res = null, data = null, code = null) {
+        let response = {};
+        if (data != null) {
+                response.data = data;
+        }
+        response.meta         = {};
+        response.meta.code    = code;
+        response.meta.message = Object.keys(CODES).filter(function (key) {
+                return CODES[ key ] === code
+        })[ 0 ];
+
+        res.send(response);
+};
+Response.prototype.ErrorWithCodeAndMessage = function (res, code, message) {
+        let response = {};
+        response.meta = {
+                code,
+                message
+        };
+        res.send(response);
+};
+module.exports                       = new Response();
