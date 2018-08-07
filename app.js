06afa11dd11e58f34381b764234efb35f4367732
Khokim Mamarasulov <alfacms.hm@gmail.com>
2018-08-07 15:04:08 +0500

    [app]: Added mongoose connection

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
+let createError  = require('http-errors');
+let express      = require('express');
+let path         = require('path');
 let cookieParser = require('cookie-parser');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
-let app = express();
+let app        = express();
+const mongoose = require('mongoose').then(data => {
+        console.log('connected to mongo db');
+});
+mongoose.connect('mongodb://localhost:27017/binary-store-backend');
 
 // view engine setup
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -18,19 +22,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+        next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error   = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
 });
 
 module.exports = app;
