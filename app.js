d49ce0418dd2990f539b20869bf476af4c5dba6a
Khokim Mamarasulov <alfacms.hm@gmail.com>
2018-09-12 16:34:52 +0500

    [Application]: Created goods

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/accounts', accountsRouter);
+app.use('/goods', accountsRouter);
 app.use('/store', storeRouter);
 app.use('/customers', customerRouter);
 
